3dbb47ee17c9dfaf25c2841638bc7a1d3da016fc
Emmanuel Adiba <eabaagah@gmail.com>
2020-11-08 04:44:39 +0000

    frontend update

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Server has started')
+app.get('/', function(req, res) {
+    res.sendFile(`${__dirname}/signup.html`);
 });
 
+app.post("/", function(req, res) {
 
+    var firstname = req.body.fname;
+    var lastname = req.body.lname;
+    var email = req.body.email;
+
+    console.log(`Hello, ${firstname} ${lastname}. Your eamil is ${email}`);
+});
 
 app.listen(port, function() {
     console.log(`Server listening at localhost:${port}`);
